9061ca68c16c55ff72d7e5cb40a60f8b3251ca07
Solgio <lorenzosoligo04@gmail.com>
2025-05-17 15:07:00 +0200

    Codice Iniziale Implementato

--- Code/code.c
+++ Code/code.c
@@ -43,45 +43,85 @@ int main(int argc, char **argv) {
     }
     printf("Connessione al database %s avvenuta con successo\n", PG_DB);
 
-    // Creazione della vista ultimo_tracking
-    const char *createViewSQL = 
-        "DROP VIEW IF EXISTS ultimo_tracking CASCADE; "
-        "CREATE VIEW ultimo_tracking (id_pacco, status) AS "
-        "SELECT t1.id_pacco, t1.status "
-        "FROM tracking t1 "
-        "INNER JOIN ("
-        "    SELECT id_pacco, MAX(data_ora) as max_data_ora "
-        "    FROM tracking "
-        "    GROUP BY id_pacco"
-        ") t2 ON t1.id_pacco = t2.id_pacco AND t1.data_ora = t2.max_data_ora;";
+printf("                                 .______    _______ .__   __. ____    ____  _______ .__   __.  __    __  .___________.  ______                                                   \n");
+printf("                                 |   _  \\  |   ____||  \\ |  | \\   \\  /   / |   ____||  \\ |  | |  |  |  | |           | /  __  \\                                                  \n");
+printf("                                 |  |_)  | |  |__   |   \\|  |  \\   \\/   /  |  |__   |   \\|  | |  |  |  | `---|  |----`|  |  |  |                                                 \n");
+printf("                                 |   _  <  |   __|  |  . `  |   \\      /   |   __|  |  . `  | |  |  |  |     |  |     |  |  |  |                                                 \n");
+printf("                                 |  |_)  | |  |____ |  |\\   |    \\    /    |  |____ |  |\\   | |  `--'  |     |  |     |  `--'  |                                                 \n");
+printf("                                 |______/  |_______||__| \\__|     \\__/     |_______||__| \\__|  \\______/      |__|      \\______/                                                  \n");
+printf("                       ___       __       __       __  __  .__   __. .___________. _______ .______       _______    ___       ______   ______  __       ___                      \n");
+printf("                      /   \\     |  |     |  |     (_ )|  | |  \\ |  | |           ||   ____||   _  \\     |   ____|  /   \\     /      | /      ||  |     /   \\                     \n");
+printf("                     /  ^  \\    |  |     |  |      |/ |  | |   \\|  | `---|  |----`|  |__   |  |_)  |    |  |__    /  ^  \\   |  ,----'|  ,----'|  |    /  ^  \\                    \n");
+printf("                    /  /_\\  \\   |  |     |  |         |  | |  . `  |     |  |     |   __|  |      /     |   __|  /  /_\\  \\  |  |     |  |     |  |   /  /_\\  \\                   \n");
+printf("                   /  _____  \\  |  `----.|  `----.    |  | |  |\\   |     |  |     |  |____ |  |\\  \\----.|  |    /  _____  \\ |  `----.|  `----.|  |  /  _____  \\                  \n");
+printf("                  /__/     \\__\\ |_______||_______|    |__| |__| \\__|     |__|     |_______|| _| `._____||__|   /__/     \\__\\ \\______| \\______||__| /__/     \\__\\                 \n");
+printf("    _______   __       _______  _______      _______..___________. __    ______   .__   __.  _______     _______   __     .______      ___       ______   ______  __    __   __  \n");
+printf("   |       \\ |  |     /  _____||   ____|    /       ||           ||  |  /  __  \\  |  \\ |  | |   ____|   |       \\ |  |    |   _  \\    /   \\     /      | /      ||  |  |  | |  | \n");
+printf("   |  .--.  ||  |    |  |  __  |  |__      |   (----``---|  |----`|  | |  |  |  | |   \\|  | |  |__      |  .--.  ||  |    |  |_)  |  /  ^  \\   |  ,----'|  ,----'|  |__|  | |  | \n");
+printf("   |  |  |  ||  |    |  | |_ | |   __|      \\   \\        |  |     |  | |  |  |  | |  . `  | |   __|     |  |  |  ||  |    |   ___/  /  /_\\  \\  |  |     |  |     |   __   | |  | \n");
+printf("   |  '--'  ||  |    |  |__| | |  |____ .----)   |       |  |     |  | |  `--'  | |  |\\   | |  |____    |  '--'  ||  |    |  |     /  _____  \\ |  `----.|  `----.|  |  |  | |  | \n");
+printf("   |_______/ |__|     \\______| |_______||_______/        |__|     |__|  \\______/  |__| \\__| |_______|   |_______/ |__|    | _|    /__/     \\__\\ \\______| \\______||__|  |__| |__| \n");
     
-    PGresult *res = PQexec(conn, createViewSQL);
-    checkCommand(res, conn);
-    PQclear(res);
-    printf("Vista ultimo_tracking creata con successo\n");
+printf("Pronto a partire?\n");
+int end=0;
+while(end!=1){
+printf("Premi invio per continuare...\n");
+getchar();
+int scelta;
+printf("Che query vuoi eseguire?\n A tua disposizione abbiamo queste 5 opzioni:\n");
+printf("\t1. Elenco dei pacchi Consegnati\n\t2. Numero pacchi per bundle con garanzia 3 anni\n\t3. Clienti con più di 2 pacchi consegnati per filiale\n\t4. Status attuale dei pacchi\n");
+printf("Qual'è la tua scelta? (1-4): ");
+scanf("%d", &scelta);
+while(scelta < 1 || scelta > 4){
+    printf("\tHai scelto molto MALE!\n\tTrovo insopportabile la sua mancanza di Input corretto\n");
+    printf("Le scelte sono 4, riprova: ");
+    scanf("%d", &scelta);
+}
+printf("\tHai scelto molto bene!  (cit)\n\n");
+if(scelta == 4){// Creazione della vista ultimo_tracking
+        const char *createViewSQL = 
+            "DROP VIEW IF EXISTS ultimo_tracking CASCADE; "
+            "CREATE VIEW ultimo_tracking (id_pacco, status) AS "
+            "SELECT t1.id_pacco, t1.status "
+            "FROM tracking t1 "
+            "INNER JOIN ("
+            "    SELECT id_pacco, MAX(data_ora) as max_data_ora "
+            "    FROM tracking "
+            "    GROUP BY id_pacco"
+            ") t2 ON t1.id_pacco = t2.id_pacco AND t1.data_ora = t2.max_data_ora;";
+        
+        PGresult *res = PQexec(conn, createViewSQL);
+        checkCommand(res, conn);
+        PQclear(res);
+        printf("Vista ultimo_tracking creata con successo\n");
 
-    // Query per selezionare i dati dalla vista
-    const char *selectSQL = "SELECT id_pacco, status FROM ultimo_tracking ORDER BY id_pacco;";
-    res = PQexec(conn, selectSQL);
-    checkResults(res, conn);
+        // Query per selezionare i dati dalla vista
+        const char *selectSQL = "SELECT id_pacco, status FROM ultimo_tracking ORDER BY id_pacco;";
+        res = PQexec(conn, selectSQL);
+        checkResults(res, conn);
 
-    // Stampa i risultati
-    int rows = PQntuples(res);
-    int cols = PQnfields(res);
-    
-    printf("\nRisultati della vista ultimo_tracking:\n");
-    printf("ID Pacco\tStatus\n");
-    printf("--------\t------\n");
-    
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%s\t\t", PQgetvalue(res, i, j));
+        // Stampa i risultati
+        int rows = PQntuples(res);
+        int cols = PQnfields(res);
+        
+        printf("\nRisultati della vista ultimo_tracking:\n");
+        printf("ID Pacco\t\t\tStatus\n");
+        printf("--------\t\t\t------\n");
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                printf("%s\t\t", PQgetvalue(res, i, j));
+            }
+            printf("\n");
         }
-        printf("\n");
+        PQclear(res);
     }
-
+    
+    printf("Vuoi continuare? (0=si, 1=no): ");
+    scanf("%d", &end);
+}
+printf("Grazie per aver usato il mio programma! Che l'algebra relazionale sia con te\n");
     // Pulizia e chiusura
-    PQclear(res);
     PQfinish(conn);
 
     return 0;
